801a07f697a4d452107bbff6bac4191a8db440c3
rfaldetta <ross.faldetta@gmail.com>
2015-08-27 01:49:41 -0500

    create interviews with two random practice questions

--- app/controllers/practice_interviews_controller.rb
+++ app/controllers/practice_interviews_controller.rb
@@ -12,11 +12,33 @@ class PracticeInterviewsController < ApplicationController
   end
 
   def create
+    if Question.count < 2
+      redirect_to "/questions", :notice => "Need at least two questions for practice interview; please add questions"
+    end
+
     @practice_interview = PracticeInterview.new
     @practice_interview.user_id = params[:user_id]
 
     if @practice_interview.save
-      redirect_to "/practice_interviews", :notice => "Practice interview created successfully."
+      a = rand(Question.count) + 1
+      b = rand(Question.count) + 1
+      while b == a
+        b = rand(Question.count) + 1
+      end
+
+      @practice_question_1 = PracticeQuestion.new
+      @practice_question_1.practice_interview_id = @practice_interview.id
+      @practice_question_1.question_id = a
+
+      @practice_question_2 = PracticeQuestion.new
+      @practice_question_2.practice_interview_id = @practice_interview.id
+      @practice_question_2.question_id = b
+
+      if @practice_question_1.save && @practice_question_2.save
+        redirect_to "/practice_interviews", :notice => "Practice interview created successfully."
+      else
+        render 'new'
+      end
     else
       render 'new'
     end
